4c188c776451fc0d10ec21dd14f5fbe8dd21a199
Tolkachev <ivan.tolkachev@infourok.biz>
2023-02-17 21:30:13 +0300

    add upload

--- sample/05-sql-typeorm/src/app.module.ts
+++ sample/05-sql-typeorm/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { QuizModule } from './quiz/quiz.module';
 import { ForTestingModule } from './for-testing-module/for-testing.module';
+import { UploadModule } from './upload/upload.module';
 
 export const typeOrmOptions: TypeOrmModuleOptions = {
   type: 'postgres',
@@ -16,6 +17,12 @@ export const typeOrmOptions: TypeOrmModuleOptions = {
 };
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmOptions), UsersModule, QuizModule, ForTestingModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmOptions),
+    UsersModule,
+    QuizModule,
+    ForTestingModule,
+    UploadModule,
+  ],
 })
 export class AppModule {}
